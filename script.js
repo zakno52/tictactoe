a26ec41ac72035345210ff4421a36f37eb5dbf6d
zakno52 <101570337+zakno52@users.noreply.github.com>
2025-01-17 22:19:20 +0100

    add restart bytton

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ const tictactoe = (function XOGame() {
   // Variables
   const data = ["X"];
   const chooseBtns = document.querySelectorAll("#chooseBtn");
+  const squares = document.querySelectorAll("td");
+  const introSection = document.querySelector(".introToggle");
+  const restartButton = document.querySelector(".restartButton");
 
   // Events
-  function addEventToChooseBtns() {
+  function addEventToButtons() {
+    // for x-o Buttons
     chooseBtns.forEach((choosebtn) => {
       choosebtn.addEventListener("click", () => saveChoice(choosebtn));
     });
+
+    // for restart button
+    restartButton.addEventListener("click", () => resetGame());
   }
 
   function addEventToEachBox() {
-    const squares = document.querySelectorAll("td");
     squares.forEach((square) => {
       square.addEventListener("click", () => handleButtonClick(square));
     });
@@ -36,6 +42,8 @@ const tictactoe = (function XOGame() {
   }
 
   function handleButtonClick(square) {
+    introSection.style.display = "none";
+    restartButton.style.display = "block";
     // data.push(square);
     const xMark = square.firstElementChild;
     const oMark = square.lastElementChild;
@@ -46,9 +54,20 @@ const tictactoe = (function XOGame() {
     }
   }
 
+  function resetGame() {
+    data.length = 0;
+    data[0] = "X";
+    squares.forEach((square) => {
+      square.firstElementChild.style.display = "none";
+      square.lastElementChild.style.display = "none";
+    });
+    restartButton.style.display = "none";
+    introSection.style.display = "block";
+  }
+
   return {
     init() {
-      addEventToChooseBtns();
+      addEventToButtons();
       addEventToEachBox();
     },
   };

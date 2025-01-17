c2d0f61b751dfa824549355152fd379994156883
zakno52 <101570337+zakno52@users.noreply.github.com>
2025-01-17 23:14:24 +0100

    add tracker

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ const tictactoe = (function XOGame() {
   const squares = document.querySelectorAll("td");
   const introSection = document.querySelector(".introToggle");
   const restartButton = document.querySelector(".restartButton");
+  const winPossibilities = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  let player1 = [];
+  let player2 = [];
+  let clickCount = 0;
 
   // Events
   function addEventToButtons() {
@@ -18,8 +31,8 @@ const tictactoe = (function XOGame() {
   }
 
   function addEventToEachBox() {
-    squares.forEach((square) => {
-      square.addEventListener("click", () => handleButtonClick(square));
+    squares.forEach((square, index) => {
+      square.addEventListener("click", () => handleButtonClick(square, index));
     });
   }
   // handler - Show results
@@ -41,7 +54,18 @@ const tictactoe = (function XOGame() {
     })();
   }
 
-  function handleButtonClick(square) {
+  function handleButtonClick(square, index) {
+    clickCount++;
+    if (clickCount % 2 === 1) {
+      // Odd click
+      player1.push(index);
+      console.log("Array1:", player1);
+    } else {
+      // Even click
+      player2.push(index);
+      console.log("Array2:", player2);
+    }
+
     introSection.style.display = "none";
     restartButton.style.display = "block";
     // data.push(square);
@@ -64,6 +88,8 @@ const tictactoe = (function XOGame() {
     restartButton.style.display = "none";
     introSection.style.display = "block";
   }
+
+  // Check winner
 
   return {
     init() {
